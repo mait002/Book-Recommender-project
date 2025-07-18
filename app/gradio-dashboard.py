0b956ba13e38bdf322d02c93599eeb4ceb26519c
mait002 <maitreyeedas02@gmail.com>
2025-07-17 23:05:12 -0400

    updated gradio-dashboard.py

--- app/gradio-dashboard.py
+++ app/gradio-dashboard.py
@@ -114,6 +114,6 @@ with gr.Blocks(theme= gr.themes.Ocean()) as dashboard:
                         outputs = output)
 
 if __name__ == "__main__":
-    dashboard.launch()
+    dashboard.launch(server_name="0.0.0.0", server_port=7860)
 
 
